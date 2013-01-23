313401444ef5ade6f5a35acc2fc059ed59e98d71
Mike McLean <libolt@libolt.net>
2013-01-22 22:40:46 -0700

    Updated Copyright and License notices.

--- src/game/basketballs.cpp
+++ src/game/basketballs.cpp
@@ -1,14 +1,22 @@
-//
-// C++ Implementation: basketballs
-//
-// Description:
-//
-//
-// Author: Mike McLean <libolt@libolt.net>, (C) 2006
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+ *   Copyright (C) 2013 by Mike McLean   *
+ *   libolt@libolt.net   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include "games.h"
 #include "ubcapp.h"
--- src/game/offense.cpp
+++ src/game/offense.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006 by Mike McLean   *
+ *   Copyright (C) 2013 by Mike McLean   *
  *   libolt@libolt.net   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
