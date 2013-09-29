5cc63d707fb8b7bf1cb1656a1104237a4f9f2e5a
Mike McLean <libolt@libolt.net>
2013-09-29 09:55:09 -0700

    Build fixes to make UBC compile with armeabi instead of armeabi-v7a. This allows it to run in the emulator and promptly crash instead of failing to load the UltimateBasketballChallenge library.

--- dependencies/tinyxml/Application.mk
+++ dependencies/tinyxml/Application.mk
@@ -2,4 +2,4 @@
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 APP_STL := gnustl_static
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi#-v7a
