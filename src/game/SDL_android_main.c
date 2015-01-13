53657e81f39620067898bf31f2e68882adf8ec19
Mike McLean <libolt@libolt.net>
2015-01-12 19:30:34 -0700

    taking another stab at android input handling

--- src/game/SDL_android_main.c
+++ src/game/SDL_android_main.c
@@ -38,6 +38,250 @@ void Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject
 }
 }
 
+
+extern "C" 
+{
+/*
+   JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
+   {
+      dish_log_lazy_init();
+      dish_log("JNI_OnLoad.");
+      dish::JniHelper& _jniHelper = dish::JniHelperManager::Instance();
+      _jniHelper.setJavaVM(vm);
+      return JNI_VERSION_1_4;
+   }
+
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_create(JNIEnv* env, jobject obj, jobject assetManager)
+   {
+      dish_log("DishNative_create.");
+      AAssetManager* _AAssetManager = AAssetManager_fromJava(env, assetManager);
+      dish::UtilFile& _file = dish::UtilFileManager::Instance();
+      _file.initialize();
+      dish::OgreRender& _render = dish::OgreRenderManager::Instance();
+      static_cast<dish::UtilFileAndroid*>(&_file)->setAAssetManager(_AAssetManager);
+      static_cast<dish::OgreRenderAndroid*>(&_render)->setAAssetManager(_AAssetManager);
+      dish::UtilLua& _lua = dish::UtilLuaManager::Instance();
+      _lua.initialize();
+   }   
+
+    JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_initWindow(JNIEnv* env, jobject obj,  jobject surface)
+   {
+      dish_log("DishNative_initWindow.");
+      dish::OgreRender& _render = dish::OgreRenderManager::Instance();
+      Ogre::RenderWindow* _renderWindow=_render.getRenderWindow();
+      ANativeWindow* _ANativeWindow = ANativeWindow_fromSurface(env, surface);
+      static_cast<dish::OgreRenderAndroid*>(&_render)->setANativeWindow(_ANativeWindow);
+      if (!_renderWindow)
+      {
+         if (surface)
+         {
+            try
+            {
+               // create the application instance
+               dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+               _app.setHandler(env);
+               _app.initialize();
+               _app.create();
+               _app.run();
+               dish_log("dish::ImplApplication end.");
+            }
+            catch (std::exception& e)
+            {
+               dish_logF("A untreated exception occur:"<<e.what());
+            }
+         }
+         else
+         {
+            dish_logE("The android surface is invalid");
+         }
+      } 
+      else
+      {
+         _render.createRenderWindow();
+      }
+   }
+   
+    JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_termWindow(JNIEnv* env, jobject obj)
+   {
+      dish::OgreRender& _render = dish::OgreRenderManager::Instance();
+      Ogre::RenderWindow* _renderWindow = _render.getRenderWindow();
+      static_cast<Ogre::AndroidEGLWindow*>(_renderWindow)->_destroyInternalResources();
+      dish_log("DishNative_termWindow");
+   }
+
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_destroy(JNIEnv* env, jobject obj)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.destroy();
+      dish_log("DishNative_destroy");
+   }
+   
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_renderOneFrame(JNIEnv* env, jobject obj)
+   {
+      dish::JniHelper& _jniHelper = dish::JniHelperManager::Instance();
+      dish::OgreRender& _render = dish::OgreRenderManager::Instance();
+      JavaVM* _javaVM = _jniHelper.getJavaVM();
+      Ogre::RenderWindow* _renderWindow = _render.getRenderWindow();
+       if(_renderWindow != NULL && _renderWindow->isActive())
+       {
+          try
+          {
+             if(_javaVM->AttachCurrentThread(&env, NULL) < 0)                
+             {
+                return;
+             }
+            
+             _renderWindow->windowMovedOrResized();
+             _render.renderOneFrame();
+            
+             //for test
+             //_javaVM->DetachCurrentThread();            
+          }
+          catch(Ogre::RenderingAPIException ex) 
+          {
+             dish_logF("A untreated exception occur:"<<ex.getFullDescription());
+          }
+       }
+   }
+
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_queueAccelerometer(JNIEnv* env, jobject obj,jfloat _x,jfloat _y,jfloat _z,jfloat _t)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.queueAccelerometer(_x,_y,_z,_t);
+   }
+
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_surfaceChanged(JNIEnv* env, jobject obj,jint _width,jint _height)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.surfaceChanged(_width,_height);
+   }
+   */
+   JNIEXPORT void JNICALL Java_com_libolt_ubc_UBCActivity_handleActionDown(JNIEnv* env, jobject obj,jint pID, jfloat pX, jfloat pY)
+   {
+/*      UBCActivity::ImplApplication& _app = UBCActivity::ImplApplicationManager::Instance();
+      _app.mousePress(pX, pY, MyGUI::MouseButton::Left);
+      //
+      _app.touchPressed(1,&pID, &pX, &pY);
+      */
+      exit(0);
+   }
+   /*
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_handleActionMove(JNIEnv* env, jobject obj,jintArray pIDs, jfloatArray pXs, jfloatArray pYs)
+   {
+        int size = env->GetArrayLength(pIDs);
+        jint id[size];
+        jfloat x[size];
+        jfloat y[size];
+
+        env->GetIntArrayRegion(pIDs, 0, size, id);
+        env->GetFloatArrayRegion(pXs, 0, size, x);
+        env->GetFloatArrayRegion(pYs, 0, size, y);
+
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.mouseMove(x[0], y[0], 0);
+      //
+      _app.touchMoved(size,id, x, y);
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_handleActionUp(JNIEnv* env, jobject obj,jint pID, jfloat pX, jfloat pY)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.mouseRelease(pX, pY, MyGUI::MouseButton::Left);
+      //
+      _app.touchReleased(1,&pID, &pX, &pY);
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_handleActionCancel(JNIEnv* env, jobject obj,jintArray pIDs, jfloatArray pXs, jfloatArray pYs)
+   {
+      int size = env->GetArrayLength(pIDs);
+      jint id[size];
+      jfloat x[size];
+      jfloat y[size];
+
+      env->GetIntArrayRegion(pIDs, 0, size, id);
+      env->GetFloatArrayRegion(pXs, 0, size, x);
+      env->GetFloatArrayRegion(pYs, 0, size, y);
+      //
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.touchCancelled(size,id, x, y);
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_onKeyDown(JNIEnv* env, jobject obj,jint pKeyCode, jint _text)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.keyPress(pKeyCode,_text);
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_onKeyUp(JNIEnv* env, jobject obj,jint pKeyCode, jint _text)
+   {
+      dish::ImplApplication& _app = dish::ImplApplicationManager::Instance();
+      _app.keyRelease(pKeyCode);
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_onPause(JNIEnv* env, jobject obj)
+   {
+      dish::AppDelegate& _app = dish::AppDelegateManager::Instance();
+      _app.enterBackgroundImpl();
+      dish_log("DishNative_onPause");
+   }
+   JNIEXPORT void JNICALL Java_org_dish_ogre_DishNative_onResume(JNIEnv* env, jobject obj)
+   {
+      dish::AppDelegate& _app = dish::AppDelegateManager::Instance();
+      _app.enterForegroundImpl();
+      dish_log("DishNative_onResume");
+   }
+   JNIEXPORT jstring JNICALL Java_org_dish_ogre_DishNative_nativeGetContentText(JNIEnv* env, jobject obj)
+   {
+      MyGUI::InputManager& _input = MyGUI::InputManager::getInstance();
+      MyGUI::Widget* _widget =  _input.getMouseFocusWidget();
+      dish_log("DishNative_nativeGetContentText");
+      if (_widget)
+      {
+         const std::string& _typeName = _widget->getTypeName();
+         if (_typeName == "EditBox")
+         {
+            MyGUI::EditBox* _editBox = static_cast<MyGUI::EditBox*>(_widget);
+            MyGUI::UString _utext = _editBox->getTextSelection();
+            const std::string& _text = _utext.asUTF8();
+            return env->NewStringUTF(_text.c_str());
+         }
+         else if(_typeName == "TextBox")
+         {
+            MyGUI::TextBox* _textBox = static_cast<MyGUI::TextBox*>(_widget);
+            MyGUI::UString _utext = _textBox->getCaption();
+            const std::string& _text = _utext.asUTF8();
+            return env->NewStringUTF(_text.c_str());
+         }
+      }
+
+      return 0;
+   }
+   JNIEXPORT jstring JNICALL Java_org_dish_ogre_DishNative_insertText(JNIEnv* env, jobject obj,jstring _text)
+   {
+      dish::JniHelper& _jh = dish::JniHelperManager::Instance();
+      std::string _textC = _jh.jstring2string(_text);
+      //
+      MyGUI::InputManager& _input = MyGUI::InputManager::getInstance();
+      MyGUI::Widget* _widget =  _input.getMouseFocusWidget();
+      dish_log("DishNative_insertText");
+      if (_widget)
+      {
+         const std::string& _typeName = _widget->getTypeName();
+         if (_typeName == "EditBox")
+         {
+            MyGUI::EditBox* _editBox = static_cast<MyGUI::EditBox*>(_widget);
+            _editBox->setCaption(_textC);
+         }
+         else if(_typeName == "TextBox")
+         {
+            MyGUI::TextBox* _textBox = static_cast<MyGUI::TextBox*>(_widget);
+            _textBox->setCaption(_textC);
+         }
+      }
+      
+   */
+};
+
+
+
+
+
+
 /*
 extern "C"
 {
