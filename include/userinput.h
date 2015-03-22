fc43704736de2b493cd7d0083c0c570a7a144b70
Mike McLean <libolt@libolt.net>
2015-03-21 21:58:37 -0700

    update

--- include/userinput.h
+++ include/userinput.h
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+ 
 #ifndef _USERINPUT_H_
 #define _USERINPUT_H_
 
--- src/game/load.cpp
+++ src/game/load.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+ 
 #ifdef __ANDROID__
 #include "android-config.h"
 #else
