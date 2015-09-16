535e2327ec991e5b8055cf6fd311835d9dfabc50
Mike McLean <libolt@libolt.net>
2015-09-16 00:09:47 -0700

    Fixed windows/msvc build issue.

--- src/game/basketballs.cpp
+++ src/game/basketballs.cpp
@@ -18,9 +18,9 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include "conversion.h"
 #include "gamestate.h"
 #include "renderengine.h"
-#include "conversion.h"
 #include "comparison.h"
 #include "logging.h"
 
